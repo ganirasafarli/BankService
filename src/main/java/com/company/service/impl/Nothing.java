b81543684600297fcfbf9de27ebdf977352bdd05
qanira.safarli <qanira.safarli@cybernet.az>
2022-04-21 16:23:34 +0400

    another implementation to creat an environment to apply several DI in the project

---
+++ src/main/java/com/company/service/impl/Nothing.java
@@ -0,0 +1,5 @@
+package com.company.service.impl;
+
+public class Nothing {
+
+}
--- src/main/java/com/company/service/impl/UserServiceImpl.java
+++ src/main/java/com/company/service/impl/UserServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 public class UserServiceImpl implements UserService {
 
-
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
